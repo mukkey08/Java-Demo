89f69308b5e1bb0c021e3e7538c1556dd3a528cf
Mukesh <info2mukeshpal@gmail.com>
2024-08-28 11:34:03 +0530

    First commit : Add some config file & change

---
+++ src/test/java/com/sample/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
